92c9ca86f4f72fbd0caee3793893494261e60d5b
Yashpal singh <yashpalsingh9rx@outlook.com>
2024-07-17 21:55:00 +0530

    setup

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:22
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+COPY tsconfig.json ./
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+CMD ["npm", "start"]
+
+EXPOSE 8000
---
+++ index.ts
@@ -0,0 +1,19 @@
+import express,{Request,Response} from 'express';
+
+const app = express();
+
+app.get('/',(req:Request,res:Response)=>{
+    res.status(200).json({message:"Alright! Boss"})
+})
+
+app.get('/health',(req,res)=>{
+    res.status(200).json({
+        running:process.uptime(),
+        time:new Date().toLocaleString()
+    })
+})
+
+
+app.listen(8000,()=>{
+    console.log("server working fine");
+})

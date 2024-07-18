dc58131077d31a7f4bda21a08008d7007097f2cf
yashpal singh <134866490+yshplsngh@users.noreply.github.com>
2024-07-18 16:28:45 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import express,{Request,Response} from 'express';
 const app = express();
 
 app.get('/',(req:Request,res:Response)=>{
-    res.status(200).json({message:"Alright! > Test 2"})
+    res.status(200).json({message:"Alright! > Test 3"})
 })
 
 app.get('/health',(req,res)=>{
